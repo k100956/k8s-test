cf21710854a8ab07b21355cd1ff8bc432dbed34d
root <root@gitlab-858616-iaas.novalocal>
2020-03-04 01:18:12 +0000

    0304

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ func main() {
 	http.HandleFunc(
 		"/hello",
 		func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintf(w, "Hello!")
+			fmt.Fprintf(w, "Hello!  20200304!!!!!!!!!!!")
 		},
 	)
 
